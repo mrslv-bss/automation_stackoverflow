9ed966bc9d4706ef6d6657c21483ce99b295f18a
mrslv-bss <bassmiroslav@gmail.com>
2022-09-06 23:17:16 +0300

    initial pages/companies functionality was deprecated and replaced by pages/default search_specific_public_page_request func for further better code readability

--- pages/companies.py
+++ pages/companies.py
@@ -1,32 +1,24 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from libs.default import send_keys, open_page_companies
+from pages.default import search_specific_public_page_request
 
-search_company_by_name = (By.XPATH, "//div[contains(@class, 'js-keyword-search')]"
+search_company_by_name_input = (By.XPATH, "//div[contains(@class, 'js-keyword-search')]"
                                     "/div/input[contains(@class, 's-input__search')]")
-search_company_by_location = (By.XPATH, "//div[contains(@class, 'js-location-search')]"
+search_company_by_location_input = (By.XPATH, "//div[contains(@class, 'js-location-search')]"
                                         "/div/input[contains(@class, 's-input__md')]")
-search_button_companies = (By.XPATH, "//button[contains(@class, 'js-search-btn')]")
-companies_search_result_number = (By.XPATH, "//div[contains(@class, 'js-search-title')]/span")
+search_companies_button = (By.XPATH, "//button[contains(@class, 'js-search-btn')]")
+companies_search_result_number_text = (By.XPATH, "//div[contains(@class, 'js-search-title')]/span")
+companies_result_grid_element = (By.XPATH, "//div[@class='company-list']/div")
 
 
 def open_companies_page_and_search_by_name(get_driver, text):
     open_page_companies(get_driver)
-    initial_number = get_driver.find_element(*companies_search_result_number).text
-    send_keys(get_driver, search_company_by_name, text)
-    send_keys(get_driver, search_company_by_name, Keys.ENTER)
-    final_number = get_driver.find_element(*companies_search_result_number).text
-    while initial_number == final_number:
-        final_number = get_driver.find_element(*companies_search_result_number).text
-    return final_number  # returns number of found companies
+    return search_specific_public_page_request(get_driver, companies_search_result_number_text,
+                                               search_company_by_name_input, companies_result_grid_element, text)
 
 
 def open_companies_page_and_search_by_location(get_driver, text):
     open_page_companies(get_driver)
-    initial_number = get_driver.find_element(*companies_search_result_number).text
-    send_keys(get_driver, search_company_by_location, text)
-    send_keys(get_driver, search_company_by_location, Keys.ENTER)
-    final_number = get_driver.find_element(*companies_search_result_number).text
-    while initial_number == final_number:
-        final_number = get_driver.find_element(*companies_search_result_number).text
-    return final_number  # returns number of found companies
+    return search_specific_public_page_request(get_driver, companies_search_result_number_text,
+                                               search_company_by_location_input, companies_result_grid_element, text)
--- pages/default.py
+++ pages/default.py
@@ -4,7 +4,7 @@ from libs.default import send_keys
 
 # often uses locators in search and after
 search_field = (By.XPATH, "//*[@class='s-topbar--searchbar--input-group']/child::input")
-search_result_array = (By.XPATH, "//*[@class='flush-left js-search-results']/child::div/div")
+search_result_array = (By.XPATH, "//*[contains(@class, 'js-search-results')]/child::div/div")
 search_result_item_name = (By.XPATH, "//*[contains(@class, 'js-search-results')]/child::div/div/div"
                                      "[@class='s-post-summary--content']/child::h3/a")
 search_result_item_desk = (By.XPATH, "//*[contains(@class, 'js-search-results')]/child::div/div/div"
@@ -17,10 +17,17 @@ search_result_item_tags = (By.XPATH, "//*[contains(@class, 'js-search-results')]
                                      "[contains(@class, 's-post-summary--meta-tags')]")
 
 
-def search_course(get_driver, text):
+def search_request(get_driver, text):
     send_keys(get_driver, search_field, text)
     send_keys(get_driver, search_field, Keys.ENTER)
-    output = get_driver.find_elements(*search_result_item_name)
-    for item in output:
-        print(item.text)
-        print("--------------------------------------------")
+    return len(get_driver.find_elements(*search_result_array))
+
+
+def search_specific_public_page_request(get_driver, validate_new_result_loc, search_input_loc, grid_result_loc, text):
+    initial_number = get_driver.find_element(*validate_new_result_loc).text
+    send_keys(get_driver, search_input_loc, text)
+    send_keys(get_driver, search_input_loc, Keys.ENTER)
+    final_number = get_driver.find_element(*validate_new_result_loc).text
+    while initial_number == final_number:
+        final_number = get_driver.find_element(*validate_new_result_loc).text
+    return len(get_driver.find_elements(*grid_result_loc))
