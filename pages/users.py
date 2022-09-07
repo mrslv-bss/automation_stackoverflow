bdb598517291829e1760522d937b9d6c9e88c9ea
mrslv-bss <bassmiroslav@gmail.com>
2022-09-07 16:17:51 +0300

    Changes in pages/users.py (open_users_page_and_search)

--- pages/users.py
+++ pages/users.py
@@ -1,18 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from libs.default import send_keys, open_page_users
+from pages.default import search_specific_public_page_request
 
-search_user_by_name = (By.ID, "userfilter")
-users_result_grid = (By.XPATH, "//div[@id='user-browser']/div/div")
-user_result_users_name = (By.XPATH, "//div[@id='user-browser']/div/div/div[@class='user-details']/a")
+search_user_by_name_input = (By.ID, "userfilter")
+users_result_grid_element = (By.XPATH, "//div[@id='user-browser']/div/div")
+user_result_users_name_text = (By.XPATH, "//div[@id='user-browser']/div/div/div[@class='user-details']/a")
 
 
 def open_users_page_and_search(get_driver, text):
     open_page_users(get_driver)
-    initial_number = get_driver.find_element(*user_result_users_name).text
-    send_keys(get_driver, search_user_by_name, text)
-    send_keys(get_driver, search_user_by_name, Keys.ENTER)
-    final_number = get_driver.find_element(*user_result_users_name).text
-    while initial_number == final_number:
-        final_number = get_driver.find_element(*user_result_users_name).text
-    return len(get_driver.find_elements(*users_result_grid))  # returns number of found users
+    return search_specific_public_page_request(get_driver, user_result_users_name_text,
+                                               search_user_by_name_input, users_result_grid_element, text)
