05eea83dd88eb512cc732ee5a707341f30fe2e1a
mrslv-bss <bassmiroslav@gmail.com>
2022-09-20 21:54:22 +0300

    search testcases suit

--- testcases/test_search.py
+++ testcases/test_search.py
@@ -1,12 +1,26 @@
-from libs.default import start, open_page_companies, open_page_tags, open_page_users
-from pages.default import search_request
+from libs.default import start
+from pages.default import search_request, click_on_grid_element
 from pages.companies import open_companies_page_and_search_by_name, open_companies_page_and_search_by_location
+from pages.users import open_users_page_and_search
+from pages.tags import open_tags_page_and_search
 
 
-def test_main(get_driver):
-    print(open_companies_page_and_search_by_location(get_driver, "ukraine"))
-
-
-def test_tagspage(get_driver):
+def test_search_request(get_driver):
     start(get_driver)
-    open_page_tags(get_driver)
+    search_request(get_driver, "python error")
+
+
+def test_search_functionality_users_page(get_driver):
+    open_users_page_and_search(get_driver, "bass_")
+
+
+def test_search_functionality_tags_page(get_driver):
+    open_tags_page_and_search(get_driver, "go")
+
+
+def test_search_functionality_companies_page_location(get_driver):
+    open_companies_page_and_search_by_location(get_driver, "ukraine")
+
+
+def test_search_functionality_companies_page_name(get_driver):
+    open_companies_page_and_search_by_name(get_driver, "qa")
