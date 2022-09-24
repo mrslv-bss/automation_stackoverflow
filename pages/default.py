21392f989a792a63c7a370186f40466289cbd7a8
mrslv-bss <bassmiroslav@gmail.com>
2022-09-24 22:11:19 +0300

    click_on_grid_element has been divided by functional for work in basic search and in public pages search (tags, users, companies). Added EC wait in search_specific_public_page_request

--- pages/default.py
+++ pages/default.py
@@ -2,6 +2,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from libs.default import send_keys, click
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 search_field = (By.XPATH, "//*[@class='s-topbar--searchbar--input-group']/child::input")
 search_result_list_type_questions = (By.XPATH, "//div[@id='questions']/div")
@@ -9,7 +11,12 @@ title_inside_a_page = (By.XPATH, "//h1//a[@class='question-hyperlink']")
 search_result_list_type_search = (By.XPATH, "//*[contains(@class, 'js-search-results')]/child::div/div")
 search_result_item_name_type_search_hyperlink = (By.XPATH, "//a[@class='s-link'] | //a[@class='answer-hyperlink ']")
 search_result_item_name_type_questions_hyperlink = (By.XPATH, "//a[@class='s-link']")
+search_result_public_users = (By.XPATH, "//div[@class='grid--item user-info  user-hover']//div[@class='user-details']//a")
+search_result_public_tags = (By.XPATH, "//div[contains(@class, 'grid--item')]//div//div[@class='flex--item']//a")
+search_result_public_company = (By.XPATH, "//div[contains(@class, 'dismissable-company')]//div//div[@class='flex--item fl1 text mb0']//h2")
 search_type_title_text = (By.XPATH, "//h1[contains(@class, 'fs-headline1')]")
+page_number_button = (By.XPATH, "//*[@class='s-pagination--item is-selected'] | "
+                                "//div[@class='py32 px16']//a[contains(text(),'Browse all companies')]")
 
 
 def search_request(get_driver, text):
@@ -22,22 +29,29 @@ def search_request(get_driver, text):
         return len(get_driver.find_elements(*search_result_list_type_search))
 
 
-def search_specific_public_page_request(get_driver, validate_new_result_loc, search_input_loc, grid_result_loc, text):
-    initial_number = get_driver.find_element(*validate_new_result_loc).text
+def search_specific_public_page_request(get_driver, search_input_loc, grid_result_loc, text):
     send_keys(get_driver, search_input_loc, text)
     send_keys(get_driver, search_input_loc, Keys.ENTER)
-    final_number = get_driver.find_element(*validate_new_result_loc).text
-    while initial_number == final_number:
-        final_number = get_driver.find_element(*validate_new_result_loc).text
+    wait = WebDriverWait(get_driver, 10)
+    wait.until(EC.presence_of_element_located((*page_number_button,)))
     return len(get_driver.find_elements(*grid_result_loc))
 
 
-def click_on_grid_element(get_driver, elem_number):
+def click_on_grid_element(get_driver, elem_number, elem_type):
+    # elem_type: True - grid elements in public pages (tags, users and companies)
+    # elem_type: False - grid elements in basic search
     searchtype = get_driver.find_element(*search_type_title_text).text
-    if "Questions tagged" in searchtype:
-        links_list = get_driver.find_elements(*search_result_item_name_type_questions_hyperlink)
+    if elem_type:
+        if "Tags" in searchtype:
+            links_list = get_driver.find_elements(*search_result_public_tags)
+        elif "Users" in searchtype:
+            links_list = get_driver.find_elements(*search_result_public_users)
+        elif "Companies" in searchtype:
+            links_list = get_driver.find_elements(*search_result_public_company)
     else:
-        links_list = get_driver.find_elements(*search_result_item_name_type_search_hyperlink)
+        if "Questions tagged" in searchtype:
+            links_list = get_driver.find_elements(*search_result_item_name_type_questions_hyperlink)
+        else:
+            links_list = get_driver.find_elements(*search_result_item_name_type_search_hyperlink)
     action = ActionChains(get_driver)
     action.move_to_element(links_list[elem_number]).click().perform()
-    return get_driver.find_element(*title_inside_a_page).text
