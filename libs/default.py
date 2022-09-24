9c3928a701a937de7a9ff62cdce13b693f5f4a4f
mrslv-bss <bassmiroslav@gmail.com>
2022-09-24 22:00:41 +0300

    Added dual_tab_start procedure + added EC waits in open_public_page

--- libs/default.py
+++ libs/default.py
@@ -1,17 +1,27 @@
 from testdata.links import url
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 companies_button = (By.ID, "nav-companies")
 tags_button = (By.ID, "nav-tags")
 users_button = (By.ID, "nav-users")
 home_container_button = (By.XPATH, "//a[contains(@class, 's-topbar--menu-btn')]")
 home_container_elements_title = (By.XPATH, "//h1[contains(@class, 'fs-headline1')]")
+home_questions_button = (By.ID, 'nav-questions')
 
 
 def start(get_driver):
     get_driver.get(url)
     get_driver.maximize_window()
     get_driver.implicitly_wait(15)
+
+
+def dual_tab_start(get_driver):
+    win1 = get_driver.window_handles[0]
+    get_driver.execute_script('window.open('');')
+    get_driver.switch_to.window(win1)
+    return get_driver.window_handles
 
 
 def click(get_driver, element):
@@ -33,6 +43,9 @@ def open_public_page(get_driver, oneofthree):
         return None
     start(get_driver)
     click(get_driver, home_container_button)
+    wait = WebDriverWait(get_driver, 10)
+    wait.until(EC.visibility_of_element_located((*home_questions_button,)))
     click(get_driver, page_button)
+    wait.until(EC.visibility_of_element_located((*home_container_elements_title,)))
     page_validation = get_driver.find_element(*home_container_elements_title).text
     assert page_validation == oneofthree
