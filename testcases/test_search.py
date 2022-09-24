7ee2ebecb7a8adde1131835946df7437f664a717
mrslv-bss <bassmiroslav@gmail.com>
2022-09-24 11:20:07 +0300

    Shortening test's name

--- testcases/test_search.py
+++ testcases/test_search.py
@@ -10,17 +10,17 @@ def test_search_request(get_driver):
     search_request(get_driver, "python error")
 
 
-def test_search_functionality_users_page(get_driver):
+def test_users_page(get_driver):
     open_users_page_and_search(get_driver, "bass_")
 
 
-def test_search_functionality_tags_page(get_driver):
+def test_tags_page(get_driver):
     open_tags_page_and_search(get_driver, "go")
 
 
-def test_search_functionality_companies_page_location(get_driver):
+def test_companies_page_location(get_driver):
     open_companies_page_and_search_by_location(get_driver, "ukraine")
 
 
-def test_search_functionality_companies_page_name(get_driver):
+def test_companies_page_name(get_driver):
     open_companies_page_and_search_by_name(get_driver, "qa")
