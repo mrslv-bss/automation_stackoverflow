1676d92d0cd55d9d78c32e0eb33be01830e20e78
mrslv-bss <bassmiroslav@gmail.com>
2022-09-25 22:30:59 +0300

    Testdata related to de0a92b.

--- testdata/options.py
+++ testdata/options.py
@@ -1,1 +1,5 @@
-click_on_element_group_by = 2
+click_on_element_group_by = 2  # test_post_search_actions.py::test_group_by_elements
+click_on_grid_element_tags = 1  # pages/tags.py::open_tags_page_and_search
+click_on_grid_element_companies = 3  # pages/companies.py::open_companies_page_and_search_by_name
+vote_counter_request = "Companies"  # test_post_search_actions.py::test_vote_counter
+search_request_data = "python error"  # test_post_search_actions.py::all
