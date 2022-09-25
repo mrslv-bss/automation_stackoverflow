5438e64e1e26179347f17c88f5a3bc21a82eb990
mrslv-bss <bassmiroslav@gmail.com>
2022-09-25 22:27:50 +0300

    Added pytest marks, pytest parametrize.

--- testcases/test_search.py
+++ testcases/test_search.py
@@ -1,26 +1,41 @@
 from libs.default import start
-from pages.default import search_request, click_on_grid_element
+from pages.default import search_request
 from pages.companies import open_companies_page_and_search_by_name, open_companies_page_and_search_by_location
 from pages.users import open_users_page_and_search
 from pages.tags import open_tags_page_and_search
+from testdata.options import search_request_data
+import pytest
+import allure
 
 
 def test_search_request(get_driver):
     start(get_driver)
-    search_request(get_driver, "python error")
+    search_request(get_driver, search_request_data)
 
 
-def test_users_page(get_driver):
-    open_users_page_and_search(get_driver, "bass_")
+@pytest.mark.public_page
+@pytest.mark.parametrize("search_data", ['user_'])
+def test_users_page(get_driver, search_data):
+    with allure.step("[1] Testcase run with params: " + search_data):
+        open_users_page_and_search(get_driver, search_data)
 
 
-def test_tags_page(get_driver):
-    open_tags_page_and_search(get_driver, "go")
+@pytest.mark.public_page
+@pytest.mark.parametrize("search_data", ['go'])
+def test_tags_page(get_driver, search_data):
+    with allure.step("[1] Testcase run with params: " + search_data):
+        open_tags_page_and_search(get_driver, search_data)
 
 
-def test_companies_page_location(get_driver):
-    open_companies_page_and_search_by_location(get_driver, "ukraine")
+@pytest.mark.public_page
+@pytest.mark.parametrize("search_data", ['ukraine'])
+def test_companies_page_location(get_driver, search_data):
+    with allure.step("[1] Testcase run with params: " + search_data):
+        open_companies_page_and_search_by_location(get_driver, search_data)
 
 
-def test_companies_page_name(get_driver):
-    open_companies_page_and_search_by_name(get_driver, "qa")
+@pytest.mark.public_page
+@pytest.mark.parametrize("search_data", ['qa'])
+def test_companies_page_name(get_driver, search_data):
+    with allure.step("[1] Testcase run with params: " + search_data):
+        open_companies_page_and_search_by_name(get_driver, search_data)
